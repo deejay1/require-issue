e788a7aee8b9b94956dfe44d57bb5c786eaa72b0
Lukasz Solniczek <lukasz.solniczek@allegro.pl>
2021-08-23 14:05:53 +0200

    MBOX-2956 add tests

--- verifier.spec.js
+++ verifier.spec.js
@@ -2,7 +2,8 @@ const Verifier = require('./verifier');
 
 const existingIssues = {
   'OPBOX-1': true,
-  'MBOX-1234': true
+  'MBOX-1234': true,
+  'L4-1309': true
 };
 
 describe('verifier', () => {
@@ -10,12 +11,16 @@ describe('verifier', () => {
   test('returns all commits having issue string that exists in tracker as valid', async () => {
     //given
     const verifier = new Verifier();
+    const commitMessages = [
+      'OPBOX-1 | testing', 'OPBOX-1 | done',
+      'L4-1309 | testing', 'L4-1309 | done'
+    ]
 
     //when
-    const results = await verifier.verifyCommitMessages(['OPBOX-1 | testing', 'OPBOX-1 | done'], async (issue) => existingIssues[issue]);
+    const results = await verifier.verifyCommitMessages(commitMessages, async (issue) => existingIssues[issue]);
 
     // expect
-    expect(results).toEqual({ invalid: [], valid: ['OPBOX-1 | testing', 'OPBOX-1 | done'], notExisting: [] });
+    expect(results).toEqual({ invalid: [], valid: commitMessages, notExisting: [] });
   });
 
   test('returns all commits having issue string that exists in tracker as valid - multiple issues in one commit', async () => {
@@ -62,12 +67,20 @@ describe('verifier', () => {
   test('returns true if pull request title includes issue string and it exists in tracker', async () => {
     //given
     const verifier = new Verifier();
+    const prTitles = [
+      'OPBOX-1 | testing',
+      'L4-1309 | testing'
+    ]
 
     //when
-    const results = await verifier.verifyTitle('OPBOX-1 | testing', async (issue) => existingIssues[issue]);
+    var results = await Promise.all(prTitles.map((title) => {
+      return verifier.verifyTitle(title, async (issue) => existingIssues[issue])
+    }));
 
     // expect
-    expect(results).toEqual(true);
+    results.forEach((result) => {
+      expect(result).toEqual(true);
+    })
   });
 
   test('returns false if pull request title includes issue string but it does not exists in tracker', async () => {
